c7fa7b51d314bd37b77ad81333a00363be47ad27
gloriafercu <gloriafercu@gmail.com>
2018-04-14 18:11:05 +0200

    refactoring

--- js/main.min.js
+++ js/main.min.js
@@ -1,8 +1,8 @@
 'use strict';
 
+var arrayTasks = [];
 var buttonAdd = document.querySelector('.button--add');
 var buttonPlus = document.querySelector('.button--plus');
-var arrayTasks = [];
 var background = document.querySelector('.background');
 
 printLocalStorage(); // Al cargar la página se imprimen las tareas que estén almacenadas previamente en el localStorage.
@@ -17,7 +17,6 @@ function printDate() {
 	var numberDate = document.querySelector('.number-date');
 	var dateWeek = document.querySelector('.date-week');
 	var monthAndYear = document.querySelector('.month-year');
-
 
 	numberDate.innerHTML = date.getDate();
 	dateWeek.innerHTML = weekdays[date.getDay()];
@@ -48,10 +47,6 @@ window.addEventListener("keydown", function(e) {
 	}
 });
 
-
-
-
-
 /* Botón(añadir) incorpora las nuevas tareas al To-Do list y regresa al estado inicial con botón(+) */
 
 function createNewTask() {
@@ -63,6 +58,7 @@ function createNewTask() {
 	};
 	document.querySelector('.new-task__input').value = '';
 	localStorageToDoList(task);
+
 }
 buttonAdd.addEventListener('click', function() {
 	createNewTask();
@@ -79,8 +75,6 @@ function localStorageToDoList(task) {
 		arrayTasks = [];
 		arrayTasks.push(task);
 		localStorage.setItem('arrayTasks', JSON.stringify(arrayTasks));
-
-
 	} else {
 		arrayTasks = JSON.parse(localStorage.getItem('arrayTasks'));
 		arrayTasks.push(task);
@@ -91,100 +85,94 @@ function localStorageToDoList(task) {
 	printLocalStorage();
 }
 
-/* Se imprimen los datos almacenados en localStorage (inclusive al cargar la página por primera vez en línea 6) */
+/* Se imprimen los datos almacenados en localStorage (inclusive al cargar la página por primera vez llamando a la función también en línea 6) */
 
 function printLocalStorage() {
-
 	var taskLocStorage = JSON.parse(localStorage.getItem('arrayTasks'));
 	if (taskLocStorage != null) {
 		var ul = document.querySelector('.todo-list');
 		var ulCompleted = document.querySelector('.done');
 		var listCompleted = '';
 		var listUncompleted = '';
-
-		taskLocStorage.reverse();
+		taskLocStorage.reverse(); // reverso del localStorage
 		for (var i = 0; i < taskLocStorage.length; i++) {
 			if (taskLocStorage[i].completed) {
 				listCompleted += '<li class="task-li strike"><input type="checkbox" checked="checked" class="checkbox" name="status" id="checkbox' + i + ' "/><label class="label-checkbox" for="checkbox' + i + '">' + taskLocStorage[i].name + '</label></li>';
-
+				// si se completa tarea se tacha la tarea y va a la lista completada
 			} else {
 				listUncompleted += '<li class="task-li "><input type="checkbox" class="checkbox" name="status" id="checkbox' + i + '"/><label class="label-checkbox" for="checkbox' + i + '">' + taskLocStorage[i].name + '</label></li>';
 			}
 		}
-
-
 		ul.innerHTML = listUncompleted;
 		ulCompleted.innerHTML = listCompleted;
 
+		// Escucha el evento click de los checkboxes de las tareas del localStorage
 		var checkboxes = document.querySelectorAll('.checkbox');
-		// console.log('checkboxes', checkboxes);
+		console.log('checkboxes', checkboxes);
 		for (var i = 0; i < checkboxes.length; i++) {
-			checkboxes[i].addEventListener('click', completeTasks);
+			checkboxes[i].addEventListener('click', changeStatusTasks);
 		}
-
 	}
 }
 
-
-/* Completar tareas terminadas */
-
-function completeTasks(event) {
+/* Marcar/Desmarcar tareas */
+function changeStatusTasks(event) {
 	var currentTask = event.currentTarget;
-	// console.log('currentTask: ', currentTask);
+	console.log('currentTask: ', currentTask);
 
 	currentTask.parentNode.classList.toggle('strike');
 	var labelTask = currentTask.nextSibling.innerHTML;
-	// console.log('labelTask: ', labelTask);
+	console.log('labelTask: ', labelTask);
 
 	var taskLocStorage = JSON.parse(localStorage.getItem('arrayTasks'));
-  // console.log('taskLocStorage: ', taskLocStorage);
+	console.log('taskLocStorage: ', taskLocStorage);
 
-	var tareaCompletada = [];
-	var posicionTareaCompletada = 0;
-	var posTareaPreviamenteMarcada = 0;
+	var taskDone = [];
+	var posiciontaskDone = 0;
+	var indexPreviouslyChecked = 0;
 
 	for (var i = 0; i < taskLocStorage.length; i++) {
 		if (labelTask === taskLocStorage[i].name) {
 			// PASO 1
-			tareaCompletada = taskLocStorage[i];
-			posicionTareaCompletada = i;
+			taskDone = taskLocStorage[i];
+			posiciontaskDone = i;
 			//Toggle del valor de la propiedad 'completed'
 			taskLocStorage[i].completed = !taskLocStorage[i].completed;
 		}
 	}
-	console.log('tareaCompletada', tareaCompletada);
-  console.log('posicionTareaCompletada: ', posicionTareaCompletada);
+	console.log('taskDone', taskDone);
+	console.log('posiciontaskDone: ', posiciontaskDone);
 
 	// PASO 2
 
-	// Eliminamos tareaCompletada del arrayAlmacen;
+	// Eliminamos taskDone del arrayAlmacen;
 	console.log('almacen original: ', taskLocStorage);
-	taskLocStorage.splice(posicionTareaCompletada,1);
+	taskLocStorage.splice(posiciontaskDone, 1);
 	console.log('almacen modificado: ', taskLocStorage);
 
 	// PASO 3
 	// Colocamos el elemento eliminado al principio del array en localStorage (aparece al final de la lista en el FRONT);
-	taskLocStorage.unshift(tareaCompletada);
+	taskLocStorage.unshift(taskDone);
 	console.log('almacen restaurado: ', taskLocStorage);
 
-  // PASO 4
+	// PASO 4
 	for (var i = 0; i < taskLocStorage.length; i++) {
 		if ((labelTask === taskLocStorage[i].name) && (taskLocStorage[i].completed)) {
-				posTareaPreviamenteMarcada = i;
+			indexPreviouslyChecked = i;
 		}
 	}
 
 	// Eliminamos tareaDesmarcada del arrayAlmacen;
- taskLocStorage.splice(posTareaPreviamenteMarcada,1);
- console.log('almacen modificado (desmarcado): ', taskLocStorage);
+	taskLocStorage.splice(indexPreviouslyChecked, 1);
+	console.log('almacen modificado (desmarcado): ', taskLocStorage);
 
- // Colocamos la tarea al final del array
- taskLocStorage.push(tareaCompletada);
- console.log('almacen restaurado (desmarcado): ', taskLocStorage);
+	// Colocamos la tarea al final del array
+	taskLocStorage.push(taskDone);
+	console.log('almacen restaurado (desmarcado): ', taskLocStorage);
 
 
 	localStorage.setItem('arrayTasks', JSON.stringify(taskLocStorage));
-	// console.log('labelTask', labelTask);
+	console.log('labelTask', labelTask);
 	printLocalStorage();
 
 }
